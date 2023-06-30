2e3404d58ea2f8cf72163471f850a5de7eaf5651
Andrés Fernández <anfernandez@fi.uba.ar>
2023-06-29 23:15:43 -0300

    adds health endpoint

--- informacion/urls.py
+++ informacion/urls.py
@@ -1,18 +1,23 @@
-
-# Django REST Framework
+# Django
 from rest_framework.routers import SimpleRouter
-
-
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import include, path
 
+# Views
 from informacion.views import PersonViewSet
 
 router = SimpleRouter()
 
-router.register(r'persons', PersonViewSet, basename='persons')
+router.register(r"persons", PersonViewSet, basename="persons")
+
+
+def health_check(request):
+    return HttpResponse("OK")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path("admin/", admin.site.urls),
+    path("health/", health_check, name="health_check"),
+    path("", include(router.urls)),
 ]

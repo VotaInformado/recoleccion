a562ab23bca2d75299f12b9e16276b1a96be909a
Andrés Fernández <anfernandez@fi.uba.ar>
2023-07-14 23:54:10 -0300

    adds redirect to healt

--- recoleccion/urls.py
+++ recoleccion/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import SimpleRouter
 from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 # Views
 from recoleccion.views import PersonViewSet
@@ -15,11 +16,16 @@ router.register(r"persons", PersonViewSet, basename="persons")
 router.register(r"deputies", DeputiesViewSet, basename="deputy_seats")
 router.register(r"senators", SenateViewSet, basename="senators")
 
+
+def redirect_to_health(request):
+    return redirect('/health/')
+
 def health_check(request):
     return HttpResponse("OK")
 
 
 urlpatterns = [
+    path("", redirect_to_health),
     path("admin/", admin.site.urls),
     path("health/", health_check, name="health_check"),
     path("", include(router.urls)),

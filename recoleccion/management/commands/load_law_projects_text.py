272ccb181911d28c27c2f2149b8ea5b63e060f14
Manuel Sturla <manuelsturla@gmail.com>
2023-08-18 21:22:24 -0300

    Refactor to use StoppableProcess

--- recoleccion/management/commands/load_law_projects_text.py
+++ recoleccion/management/commands/load_law_projects_text.py
@@ -17,18 +17,9 @@ from multiprocessing import Process, Queue, Event, current_process, active_child
 from queue import Empty
 from time import sleep
 import argparse
+from recoleccion.components.process import StoppableProcess
 
 DEFAULT_PROCESS_NUMBER = 5
-
-
-def process(func):
-    def wrapper_func(*args, **kwargs):
-        try:
-            func(*args, **kwargs)
-        except KeyboardInterrupt:
-            pass  # To avoid printing the stack trace when force stopping the process
-
-    return wrapper_func
 
 
 class Command(BaseCommand):
@@ -40,53 +31,51 @@ class Command(BaseCommand):
         parser.add_argument("--processes", type=int, default=DEFAULT_PROCESS_NUMBER)
         parser.add_argument("--only-missing", action=argparse.BooleanOptionalAction)
 
-    @process
-    def worker(self, projects_queue, data_queue, stop_event):
+    def worker(self, projects_queue, data_queue):
         this_process = current_process().name
-        while not stop_event.is_set():
-            try:
-                project = projects_queue.get(timeout=2)
-                if project.origin_chamber == ProjectChambers.DEPUTIES:
-                    num, source, year = project.deputies_project_id.split("-")
-                    text, link = DeputiesLawProyectsText.get_text(num, source, year)
-                    data_queue.put((project, text, link))
-                elif project.origin_chamber == ProjectChambers.SENATORS:
-                    parts = project.senate_project_id.split("-")
-                    # Ugly FIX: some projects have a wrong format TODO: fix
-                    if len(parts) == 3:
-                        num, source, year = parts
-                    elif len(parts) == 2:
-                        num, year = parts
-                        source = "S"
-                    else:
-                        continue
-                    text, link = SenateLawProyectsText.get_text(num, source, year)
-                    # TODO: if text is empty try to get text with deputies Projects source
-                    data_queue.put((project, text, link))
-            except Empty:
-                self.logger.info(f"{this_process} > Projects queue empty")
-                break
+        try:
+            project = projects_queue.get(timeout=2)
+            if project.origin_chamber == ProjectChambers.DEPUTIES:
+                num, source, year = project.deputies_project_id.split("-")
+                text, link = DeputiesLawProyectsText.get_text(num, source, year)
+                data_queue.put((project, text, link))
+            elif project.origin_chamber == ProjectChambers.SENATORS:
+                parts = project.senate_project_id.split("-")
+                # Ugly FIX: some projects have a wrong format TODO: fix
+                if len(parts) == 3:
+                    num, source, year = parts
+                elif len(parts) >= 2:
+                    num = parts[0]
+                    year = parts[-1]
+                    source = "S"
+                else:
+                    self.logger.error(
+                        f"{this_process} > Invalid project id: {project.senate_project_id}"
+                    )
+                    return False
+                text, link = SenateLawProyectsText.get_text(num, source, year)
+                # TODO: if text is empty try to get text with deputies Projects source
+                data_queue.put((project, text, link))
+        except Empty:
+            self.logger.info(f"{this_process} > Projects queue empty")
+            return True
 
-    @process
-    def writer(self, data_queue, stop_event):
-        while not stop_event.is_set():
-            try:
-                data = data_queue.get(timeout=1)
-                project, text, link = data
-                self.logger.info(
-                    f"Writer > Updating Project [id:{project.id}, deputy_id:{project.deputies_project_id}, senate_id:{project.senate_project_id}] "
-                )
-                text = text.replace("\x00", "\uFFFD")
-                link = link.replace("\x00", "\uFFFD")
-                LawProject.update(id=project.id, text=text, link=link)
-                self.logger.info(
-                    f"Writer > Correctly updated Project [id:{project.id}, deputy_id:{project.deputies_project_id}, senate_id:{project.senate_project_id}]"
-                )
-            except Empty:
-                pass
-            except Exception as e:
-                self.logger.error(f"Writer > Error: {repr(e)}")
-        self.logger.info(f"Writer > Stopped")
+    def writer(self, data_queue):
+        this_process = current_process().name
+        try:
+            data = data_queue.get(timeout=1)
+            project, text, link = data
+            self.logger.info(
+                f"{this_process} > Updating Project [id:{project.id}, deputy_id:{project.deputies_project_id}, senate_id:{project.senate_project_id}] "
+            )
+            text = text.replace("\x00", "\uFFFD")
+            link = link.replace("\x00", "\uFFFD")
+            LawProject.update(id=project.id, text=text, link=link)
+            self.logger.info(
+                f"{this_process} > Correctly updated Project [id:{project.id}, deputy_id:{project.deputies_project_id}, senate_id:{project.senate_project_id}]"
+            )
+        except Empty:
+            pass  # To avoid locking into empty queue
 
     def handle(self, *args, **options):
         self.num_processes = options.get("processes", self.num_processes)
@@ -102,61 +91,54 @@ class Command(BaseCommand):
         for project in projects:
             self.projects_queue.put(project)
         self.workers = []
-        self.stop_writer = Event()
-        self.stop_workers = Event()
-        self.writer_thread = Process(
-            target=self.writer, args=(self.data_queue, self.stop_writer), name="Writer"
+        self.writer_thread = StoppableProcess(
+            target=self.writer, args=(self.data_queue,), name="Writer"
         )
-        self.writer_thread.start()
         try:
             for i in range(self.num_processes):
-                t = Process(
+                p = StoppableProcess(
                     target=self.worker,
-                    args=(self.projects_queue, self.data_queue, self.stop_workers),
+                    args=(self.projects_queue, self.data_queue),
                     name=f"Worker-{i}",
                 )
-                self.workers.append(t)
-                t.start()
+                self.workers.append(p)
             alive_workers = True
             while alive_workers:
                 self.logger.info(
                     f"CURRENT STATE: \n\t- Projects queue size: {self.projects_queue.qsize()}\n\t- Data queue size: {self.data_queue.qsize()}"
                 )
-                alive_workers = [t.name for t in self.workers if t.is_alive()]
+                alive_workers = [t.name for t in self.workers if t.stopped()]
                 self.logger.info(f"Alive workers: {str(alive_workers)}")
                 sleep(1)
             self.logger.warning("No workers alive, explicitly stopping them...")
-            self.stop_workers.set()
-            for t in self.workers:
-                t.join()
+            for p in self.workers:
+                p.stop_and_join()
             self.logger.info("All workers stopped")
-            while not self.data_queue.empty():
+            while not self.data_queue.empty():  # TODO: unreliable
                 self.logger.info(
                     f"Working...\n\t- Projects queue size: {self.projects_queue.qsize()}\n\t- Data queue size: {self.data_queue.qsize()}"
                 )
                 sleep(1)
-            self.stop_writer.set()
-            self.writer_thread.join()
+            self.writer_thread.stop_and_join()
             self.logger.info("All processes stopped")
-        except:
-            self.logger.error("Exception occurred. Stopping threads...")
-            self.logger.info(
-                f"Projects remaining in workers queue: {self.projects_queue.qsize()}"
-            )
-            self.logger.info(
-                f"Data remaining in writer queue: {self.data_queue.qsize()}"
-            )
+        except KeyboardInterrupt:
+            self.logger.warning("Keyboard interrupt received. Stopping threads...")
+            self._stop_threads()
+        except BaseException as e:
+            self.logger.error(f"Exception occurred: {repr(e)}. Stopping threads...")
             self._stop_threads()
 
     def _stop_threads(self):
+        self.logger.info(
+            f"Projects remaining in workers queue: {self.projects_queue.qsize()}"
+        )
+        self.logger.info(f"Data remaining in writer queue: {self.data_queue.qsize()}")
         # To dont need to flush queues
         self.projects_queue.cancel_join_thread()
         self.data_queue.cancel_join_thread()
-        self.stop_workers.set()
-        for t in self.workers:
-            t.join()
+        for p in self.workers:
+            p.stop_and_join()
         self.logger.info("Workers joined")
-        self.stop_writer.set()
-        self.writer_thread.join()
+        self.writer_thread.stop_and_join()
         self.logger.info("Writer joined")
         self.logger.info("All processes stopped")

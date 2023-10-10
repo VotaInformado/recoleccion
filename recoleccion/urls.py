feb4357c068efaf2c85bbefbafcb819b9cd12102
Manuel Sturla <manuelsturla@gmail.com>
2023-10-10 18:03:03 -0300

    Add law projects votings view

--- recoleccion/urls.py
+++ recoleccion/urls.py
@@ -14,12 +14,12 @@ from recoleccion.views.deputies import DeputiesViewSet
 from recoleccion.views.legislators import LegislatorsViewSet
 from recoleccion.views.senate import SenateViewSet
 from recoleccion.views.laws import LawsViewSet
-from recoleccion.views.law_projects import LawProjectsViewSet
+from recoleccion.views.law_projects import LawProjectsViewSet, LawProyectVotesViewSet
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Your API",
-        default_version='v1',
+        default_version="v1",
         description="Your API description",
         terms_of_service="https://www.yourapp.com/terms/",
         contact=openapi.Contact(email="contact@yourapp.com"),
@@ -38,10 +38,16 @@ router.register(r"deputies", DeputiesViewSet, basename="deputy_seats")
 router.register(r"senators", SenateViewSet, basename="senators")
 router.register(r"laws", LawsViewSet, basename="laws")
 router.register(r"law-projects", LawProjectsViewSet, basename="law-project")
+router.register(
+    r"law-projects/(?P<law_project_id>[^/.]+)/votings",
+    LawProyectVotesViewSet,
+    basename="law-project-votings",
+)
 
 
 def redirect_to_health(request):
-    return redirect('/health/')
+    return redirect("/health/")
+
 
 def health_check(request):
     return HttpResponse("OK")
@@ -52,6 +58,12 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("health/", health_check, name="health_check"),
     path("", include(router.urls)),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),  # To download the swagger.json
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),  # To download the swagger.json
 ]
--- recoleccion/views/law_projects.py
+++ recoleccion/views/law_projects.py
@@ -1,13 +1,24 @@
 # Django rest framework
-from rest_framework import viewsets, mixins
-from recoleccion.serializers.law_projects import LawProjectListSerializer, LawProjectRetrieveSerializer
+from rest_framework import viewsets, mixins, generics
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from recoleccion.serializers.law_projects import (
+    LawProjectListSerializer,
+    LawProjectRetrieveSerializer,
+)
+from recoleccion.serializers.votes import VoteModelSerializer
+from recoleccion.views.responses.law_projects import law_project_votings_responses
 
 # Models
 from recoleccion.models import LawProject
 from recoleccion.utils.enums.project_chambers import ProjectChambers
 
 
-class LawProjectsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+class LawProjectsViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin
+):
     def get_serializer_class(self):
         if self.action == "list":
             return LawProjectListSerializer
@@ -22,3 +33,20 @@ class LawProjectsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.
         "publication_date": ["gte", "lte"],
     }
     ordering_fields = ["title", "publication_date", "status"]
+
+
+class LawProyectVotesViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    serializer_class = VoteModelSerializer
+
+    filterset_fields = {
+        "chamber": ["exact"],
+        "date": ["exact"],
+        "vote": ["exact", "in"],
+    }
+    ordering_fields = ["vote"]
+    search_fields = ["person__name", "person__last_name", "vote"]
+
+    def get_queryset(self):
+        law_project_id = self.kwargs["law_project_id"]
+        law_project = LawProject.objects.get(id=law_project_id)
+        return law_project.votes.all()

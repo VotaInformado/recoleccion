7a3bcf1375186a77ec2d3b0888e559ef6cfae112
Andrés Fernández <anfernandez@fi.uba.ar>
2024-03-04 00:14:54 -0300

    seat serializer now returns party.denomination instead of party_name value

--- recoleccion/serializers/senate.py
+++ recoleccion/serializers/senate.py
@@ -2,15 +2,19 @@
 from rest_framework import serializers
 
 # Models
-from recoleccion.models import SenateSeat
+from recoleccion.models import DeputySeat, SenateSeat
 
 
 class ReducedSenateSeatSerializer(serializers.ModelSerializer):
     chamber = serializers.ReadOnlyField(default="Senado")
+    party_name = serializers.SerializerMethodField()
 
     class Meta:
         model = SenateSeat
         exclude = ["person"]
+
+    def get_party_name(self, obj: DeputySeat | SenateSeat):
+        return obj.party.main_denomination if obj.party else obj.party_name
 
 
 class SenateSeatModelSerializer(serializers.ModelSerializer):

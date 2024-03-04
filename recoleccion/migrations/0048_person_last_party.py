635a119ca8a1b4614c86a53c3a7bec715f220c7a
Manuel Sturla <manuelsturla@gmail.com>
2024-03-04 19:21:26 -0300

    fix migration 048

--- recoleccion/migrations/0048_person_last_party.py
+++ recoleccion/migrations/0048_person_last_party.py
@@ -2,18 +2,9 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
-from recoleccion.models import Person as PersonModel
-from recoleccion.models.party import Party
 
 
 class Migration(migrations.Migration):
-
-    def set_last_party(apps, schema_editor):
-        Person: PersonModel = apps.get_model("recoleccion", "Person")
-        for person in Person.objects.all():
-            party: Party = person.get_last_party()
-            person.last_party = party.main_denomination if party else None
-            person.save()
 
     dependencies = [
         ("recoleccion", "0047_deputyseat_source_law_source_missingrecord_source_and_more"),
@@ -25,5 +16,4 @@ class Migration(migrations.Migration):
             name="last_party",
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to="recoleccion.party"),
         ),
-        migrations.RunPython(set_last_party),
     ]
